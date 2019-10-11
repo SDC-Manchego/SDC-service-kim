a30b8a7136c8413e2f4677dc6bbd388ccd87df5c
David Fuentes <D.gabefuentes@gmail.com>
2019-10-10 17:59:08 -0700

    Implements seed.js and script in package.json

--- database/index.js
+++ database/index.js
@@ -7,24 +7,15 @@ db.once('open', () => {
   console.log('Connected to mongoDB!');
 });
 
-// define mongoose schema
+
 let photoSchema = mongoose.Schema({
   product: String,
+  color: String,
   image_url: String
 });
 
 let Photos = mongoose.model('Photos', photoSchema);
 
-
-let photo1 = new Photos({product: 'Ikea Thing lol', image_url: 'https://davidfuentsmedia.s3-us-west-1.amazonaws.com/ikeaBlack1.webp'});
-
-photo1.save((err, photo) => {
-  if(err) {
-    return console.log(err);
-  } else {
-    console.log('Aylmaowedidit');
-  }
-});
 
 
 
