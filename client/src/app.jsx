7eff43642d04ee03d211d60dceeaa938a07d6103
David Fuentes <D.gabefuentes@gmail.com>
2019-10-15 11:16:15 -0700

    Fetches photos from server/db and renders onto page. Also implements previous/next button

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,19 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import Card from './card.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      initial: 'Hello World!'
+      mainPic: '',
+      photos: []
     }
   }
+
+  componentDidMount() {
+    this.getAllPhotos();
+  }
+
+  getAllPhotos() {
+    $.ajax({
+      url: '/api/photos',
+      type: 'GET',
+      success: (photos) => {
+        console.log('DEEEZ DEM PHOTOS ---->', photos);
+        console.log('mainPic???--->', photos[0].image_url);
+        this.setState({
+          mainPic: photos[0],
+          photos: photos
+        });
+      }
+    });
+  }
+  // click handler for changing mainpic based off smolpic (need to make component now)
+  // changeMainPic() {
+  //   this.setState({
+  //     mainPic: this.props.image_url
+  //   });
+  // }
+  nextProp () {
+    const newIdx = this.state.mainPic.index+1
+    this.setState({
+      mainPic: this.state.photos[newIdx]
+    })
+  }
+
+  prevProp () {
+    const newIdx = this.state.mainPic.index-1;
+    this.setState({
+      mainPic: this.state.photos[newIdx]
+    })
+  }
+
+
 
   render() {
     return(
       <div>
-        <h1>{this.state.initial}</h1>
+        <button
+        onClick={() => this.prevProp()}
+        disabled={this.state.mainPic.index === 0}
+        >Prev</button>
+        <button
+        onClick={() => this.nextProp()}
+        disabled={this.state.mainPic.index === this.state.photos.length-1}
+        >Next</button>
+        <Card pic={this.state.mainPic}/>
+        {this.state.photos.map((photo) =>
+          <img src={photo.image_url}/>)}
       </div>
     )
   }
