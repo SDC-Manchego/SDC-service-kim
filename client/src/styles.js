4b412fdddbe18c3f338aba2fe45641f9e65a774a
David Fuentes <D.gabefuentes@gmail.com>
2019-10-20 19:41:45 -0700

    Implements functional animations/conditionally rendered zoom.jsx

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,19 +1,14 @@
 const styles = {
   mainCard: {
     height: '564px',
-    width: '564px'
-  },
-  smolCard: {
-    height: '62px',
-    width: '62px',
-    margin: '10px 0px 5px 0px',
-    border: '1px solid #ccc'
+    width: '564px',
+    cursor: 'zoom-in'
   },
   productContainer: {
     display: 'flex',
     flexDirection: 'row',
     height: '858.73px',
-    width: '764px',
+    width: '964px',
     alignItems: 'center'
   },
   leftGroup: {
@@ -29,6 +24,45 @@ const styles = {
     margin: '0px 20px 0px 20px',
     fontSize: '25px',
     outline: 'none'
+  },
+  zoomList: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 'auto',
+    width: 'auto',
+    alignItems: 'center',
+
+  },
+  zoomImage: {
+    height: '1100px',
+    width: '1100px',
+    marginTop: '5px'
+  },
+  exit: {
+    position: 'fixed',
+    top: '2%',
+    right: '2%'
+  },
+  xButton: {
+    height: '10px',
+    width: '50px',
+    border: 'none',
+    margin: '0px 20px 0px 20px',
+    fontSize: '25px',
+    outline: 'none',
+    cursor: 'pointer'
+  },
+  smolCard: {
+    height: '62px',
+    width: '62px',
+    margin: '10px 0px 5px 0px',
+    border: '1px solid #ccc'
+  },
+  slider: {
+    overflow: 'hidden',
+    height: '564px',
+    width: '564px',
+    position: 'relative'
   }
 }
 
